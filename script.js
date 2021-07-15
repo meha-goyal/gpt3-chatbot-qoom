ac4143ce7b53a0d21099740b016beec3c246def5
Meha Goyal <64508470+MehaG1@users.noreply.github.com>
2021-07-14 18:29:05 -0700

    Add HTML/CSS and JS files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function() {
+    $(document).delegate(".chat-btn", "click", function() {
+    var value = $(this).attr("chat-value");
+    var name = $(this).html();
+    $("#chat-input").attr("disabled", false);
+  })
+  
+  $("#chat-circle").click(function() {    
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+  
+  $(".chat-box-toggle").click(function() {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+  
+})
